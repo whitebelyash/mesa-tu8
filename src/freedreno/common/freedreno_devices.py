4b1954d87a05ba8d983c504ab912de4804a1ca66
whitebelyash <whbexiumwork@gmail.com>
2026-01-06 20:19:59 +0400

    freedreno: split A830 & A830v2 definitions
    These are gen8_0_0 and gen8_0_1 in KGSL, but generally are same
    
    Signed-off-by: whitebelyash <whbexiumwork@gmail.com>

--- src/freedreno/common/freedreno_devices.py
+++ src/freedreno/common/freedreno_devices.py
@@ -1471,7 +1471,29 @@ a8xx_gen2 = GPUProps(
 
 # Totally fake, just to get cffdump to work:
 add_gpus([
-        GPUId(chip_id=0x44050001, name="FD830"),
+        GPUId(chip_id=0x44050000, name="FD830"),
+    ], A6xxGPUInfo(
+        CHIP.A8XX,
+        [a7xx_base, a7xx_gen3, a8xx_base],
+        num_ccu = 6,
+        num_slices = 3,
+        tile_align_w = 64,
+        tile_align_h = 32,
+        tile_max_w = 16384,
+        tile_max_h = 16384,
+        num_vsc_pipes = 32,
+        cs_shared_mem_size = 32 * 1024,
+        wave_granularity = 2,
+        fibers_per_sp = 128 * 2 * 16,
+        magic_regs = dict(
+        ),
+        raw_magic_regs = [
+        ],
+    ))
+
+# Not really sure how it's different from the upper one:
+add_gpus([
+        GPUId(chip_id=0x44050001, name="FD830v2"),
     ], A6xxGPUInfo(
         CHIP.A8XX,
         [a7xx_base, a7xx_gen3, a8xx_base],
