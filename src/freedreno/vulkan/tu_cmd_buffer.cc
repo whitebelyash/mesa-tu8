8cbf2081df43af2c9cba26025b31a06d0f8685a4
whitebelyash <whbexiumwork@gmail.com>
2026-01-13 01:17:37 +0400

    HACK: tu: disable gmem rendering for A830
    It's broken till upstream fixes A830
    
    Signed-off-by: whitebelyash <whbexiumwork@gmail.com>

--- src/freedreno/vulkan/tu_cmd_buffer.cc
+++ src/freedreno/vulkan/tu_cmd_buffer.cc
@@ -1351,6 +1351,13 @@ use_sysmem_rendering(struct tu_cmd_buffer *cmd,
       cmd->state.rp.gmem_disable_reason = "TU_DEBUG(SYSMEM)";
       return true;
    }
+   
+   /* TODO: improve the check */
+   uint64_t chip_id = cmd->device->physical_device->dev_id.chip_id;
+   if (chip_id == 0x44050000 || chip_id == 0x44050001) {
+      cmd->state.rp.gmem_disable_reason = "Unsupported GPU";
+      return true;
+   }
 
    /* can't fit attachments into gmem */
    if (!cmd->state.tiling->possible) {
