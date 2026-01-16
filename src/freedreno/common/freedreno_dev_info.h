fdb8f9e5fafbbb0bfe65fe0ce2dbe44c0aeb1223
whitebelyash <whbexiumwork@gmail.com>
2026-01-16 22:55:20 +0400

    tu/freedreno: add disable_gmem GPU property
    Disables GMEM usage completely, effectively same thing as TU_DEBUG=sysmem
    
    Signed-off-by: whitebelyash <whbexiumwork@gmail.com>

--- src/freedreno/common/freedreno_dev_info.h
+++ src/freedreno/common/freedreno_dev_info.h
@@ -422,6 +422,10 @@ struct fd_dev_info {
        * expected:
        */
       bool has_salu_int_narrowing_quirk;
+      /* Whether the device supports the image processing opcode */
+      bool has_image_processing;
+      /* If GMEM needs to be disabled for this GPU */
+      bool disable_gmem;
    } props;
 };
 
