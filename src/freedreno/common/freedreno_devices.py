5d721587fa9c21ef06ac47a277419d74d5892d48
whitebelyash <whbexiumwork@gmail.com>
2026-03-02 13:32:14 +0400

    freedreno/common: enable Adreno 810/825/829
    Based on upstream MR patches and some of my work. Pretty much untested
    as I don't have neither of those GPUs

--- src/freedreno/common/freedreno_devices.py
+++ src/freedreno/common/freedreno_devices.py
@@ -1438,7 +1438,9 @@ a8xx_base = GPUProps(
         has_dp2acc = False,
         reg_size_vec4 = 96,
         has_rt_workaround = False,
-        supports_double_threadsize = False
+        supports_double_threadsize = False,
+        has_dual_wave_dispatch = True,
+        has_salu_int_narrowing_quirk = True,
     )
 
 # For a8xx, the chicken bit and most other non-ctx reg
@@ -1491,8 +1493,6 @@ a8xx_gen1 = GPUProps(
         gmem_per_ccu_color_cache_size = 16 * 1024,
         gmem_ccu_depth_cache_fraction = CCUColorCacheFraction.FULL.value,
         gmem_per_ccu_depth_cache_size = 256 * 1024,
-
-        has_salu_int_narrowing_quirk = True
 )
 
 a8xx_gen2 = GPUProps(
@@ -1512,13 +1512,11 @@ a8xx_gen2 = GPUProps(
         gmem_ccu_depth_cache_fraction = CCUColorCacheFraction.FULL.value,
         gmem_per_ccu_depth_cache_size = 256 * 1024,
         has_fs_tex_prefetch = False,
-
-        has_salu_int_narrowing_quirk = True
 )
 
 add_gpus([
-        GPUId(chip_id=0x44050000, name="FD830"),
-        GPUId(chip_id=0x44050001, name="FD830"), # KGSL
+        GPUId(chip_id=0x44050000, name="Adreno (TM) 830"),
+        GPUId(chip_id=0x44050001, name="Adreno (TM) 830"), # KGSL
     ], A6xxGPUInfo(
         CHIP.A8XX,
         [a7xx_base, a7xx_gen3, a8xx_base, a8xx_gen1],
@@ -1535,6 +1533,84 @@ add_gpus([
         magic_regs = dict(),
         raw_magic_regs = a8xx_base_raw_magic_regs,
     ))
+
+# gen8_3_0
+add_gpus([
+        GPUId(chip_id=0x44010000, name="Adreno (TM) 810"),
+    ], A6xxGPUInfo(
+        CHIP.A8XX,
+        [a7xx_base, a7xx_gen3, a8xx_base, a8xx_gen2, GPUProps(
+            gmem_vpc_attr_buf_size = 16384,
+            gmem_vpc_pos_buf_size = 12288,
+            gmem_vpc_bv_pos_buf_size = 20480,
+            # Don't show "raytracing disabled"
+            has_ray_intersection = False,
+            has_sw_fuse = False,
+         )],
+        num_ccu = 1, # I'm not sure about this
+        num_slices = 1,
+        tile_align_w = 32,
+        tile_align_h = 16,
+        tile_max_w = 16384,
+        tile_max_h = 16384,
+        num_vsc_pipes = 32,
+        cs_shared_mem_size = 32 * 1024,
+        wave_granularity = 2,
+        fibers_per_sp = 128 * 2 * 16,
+        magic_regs = dict(),
+        raw_magic_regs = a8xx_base_raw_magic_regs,
+    ))
+
+# gen8_6_0
+add_gpus([
+        GPUId(chip_id=0x44030000, name="Adreno (TM) 825"),
+    ], A6xxGPUInfo(
+        CHIP.A8XX,
+        [a7xx_base, a7xx_gen3, a8xx_base, a8xx_gen1, GPUProps(
+            # This is probably not an optimal config for gmem/sysmem, but it was working before and I don't have any a825 device to test (neither I have any trace info)
+            sysmem_ccu_color_cache_fraction = CCUColorCacheFraction.FULL.value,
+            sysmem_per_ccu_color_cache_size = 128 * 1024,
+            sysmem_ccu_depth_cache_fraction = CCUColorCacheFraction.THREE_QUARTER.value,
+            sysmem_per_ccu_depth_cache_size = 96 * 1024,
+        )],
+        num_ccu = 4,
+        num_slices = 2,
+        tile_align_w = 96,
+        tile_align_h = 32,
+        tile_max_w = 16416,
+        tile_max_h = 16384,
+        num_vsc_pipes = 32,
+        cs_shared_mem_size = 32 * 1024,
+        wave_granularity = 2,
+        fibers_per_sp = 128 * 2 * 16,
+        magic_regs = dict(),
+        raw_magic_regs = a8xx_base_raw_magic_regs,
+    ))
+
+# ??????
+add_gpus([
+    GPUId(chip_id=0x44030A00, name="Adreno (TM) 829"), # KGSL
+    GPUId(chip_id=0x44030A20, name="Adreno (TM) 829"), # KGSL - found by testing, another revision?
+    GPUId(chip_id=0xffff44030A00, name="Adreno (TM) 829"),
+    ], A6xxGPUInfo(
+        CHIP.A8XX,
+        [a7xx_base, a7xx_gen3, a8xx_base, a8xx_gen2, GPUProps(
+            sysmem_vpc_bv_pos_buf_size = 24576,
+        )],
+        num_ccu = 4,
+        num_slices = 2,
+        tile_align_w = 64,
+        tile_align_h = 32,
+        tile_max_w = 16384,
+        tile_max_h = 16384,
+        num_vsc_pipes = 32,
+        cs_shared_mem_size = 32 * 1024,
+        wave_granularity = 2,
+        fibers_per_sp = 128 * 2 * 16,
+        magic_regs = dict(),
+        raw_magic_regs = a8xx_base_raw_magic_regs,
+    ))
+
 
 add_gpus([
         GPUId(chip_id=0xffff44050A31, name="Adreno (TM) 840"),
