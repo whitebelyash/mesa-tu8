309c23d80bf99f29cc8f1c3dc5fc1a0adf7e502e
DVD <dmitrijnemov13@gmail.com>
2026-03-31 20:29:31 +0300

    Update freedreno_devices.py
    Signed-off-by: DVD  <dmitrijnemov13@gmail.com>

--- src/freedreno/common/freedreno_devices.py
+++ src/freedreno/common/freedreno_devices.py
@@ -1372,10 +1372,38 @@ a8xx_gen2 = GPUProps(
 
 add_gpus([
         GPUId(chip_id=0xffff44050000, name="Adreno (TM) 830"),
-        GPUId(chip_id=0x44050001, name="Adreno (TM) 830"), # KGSL
+        GPUId(chip_id=0x44050001, name="Adreno (TM) 830"), 
     ], A6xxGPUInfo(
         CHIP.A8XX,
-        [a7xx_base, a7xx_gen3, a8xx_base, a8xx_gen1],
+        [a7xx_base, a7xx_gen3, a8xx_base, a8xx_gen1, 
+         GPUProps(
+             # Sysmem буфферы
+            sysmem_vpc_attr_buf_size = 1572864, 
+            sysmem_vpc_pos_buf_size = 786432,
+            sysmem_vpc_bv_pos_buf_size = 393216,
+# глубины цветов sysmem
+            sysmem_ccu_color_cache_fraction = CCUColorCacheFraction.FULL.value,
+            sysmem_per_ccu_color_cache_size = 256 * 1024,
+            sysmem_ccu_depth_cache_fraction = CCUColorCacheFraction.THREE_QUARTER.value,
+            sysmem_per_ccu_depth_cache_size = 256 * 1024,
+#Gmem глубина и цвет 
+            gmem_ccu_color_cache_fraction = CCUColorCacheFraction.EIGHTH.value,
+            gmem_per_ccu_color_cache_size = 256 * 1024,
+            gmem_ccu_depth_cache_fraction = CCUColorCacheFraction.FULL.value,
+            gmem_per_ccu_depth_cache_size = 512 * 1024,
+            
+            #Gmem буфферы
+            gmem_vpc_attr_buf_size = 196608,
+            gmem_vpc_pos_buf_size = 98304,
+            gmem_vpc_bv_pos_buf_size = 98304,
+             
+            gmem_size = 12 * 1024 * 1024,
+            has_ray_intersection = False,
+            has_sw_fuse = False,
+            has_coherent_ubwc_flag_caches = True,
+            has_fs_tex_prefetch = False,
+            has_salu_int_narrowing_quirk = True,
+         )],
         num_ccu = 6,
         num_slices = 3,
         tile_align_w = 96,
@@ -1444,6 +1472,7 @@ add_gpus([
     ], A6xxGPUInfo(
         CHIP.A8XX,
         [a7xx_base, a7xx_gen3, a8xx_base, a8xx_gen1, GPUProps(
+            # Ждем фикса от DerixSpace 
             # This is probably not an optimal config for gmem/sysmem, but it was working before and I don't have any a825 device to test (neither I have any trace info)
             sysmem_ccu_color_cache_fraction = CCUColorCacheFraction.FULL.value,
             sysmem_per_ccu_color_cache_size = 128 * 1024,
@@ -1520,7 +1549,34 @@ add_gpus([
     ], A6xxGPUInfo(
         CHIP.A8XX,
         [a7xx_base, a7xx_gen3, a8xx_base, a8xx_gen2,
-         GPUProps(shading_rate_matches_vk = True)],
+         GPUProps(
+                 # Sysmem буфферы
+            sysmem_vpc_attr_buf_size = 2097152, 
+            sysmem_vpc_pos_buf_size = 1048576,
+            sysmem_vpc_bv_pos_buf_size = 524288,
+# глубины цветов sysmem
+            sysmem_ccu_color_cache_fraction = CCUColorCacheFraction.FULL.value,
+            sysmem_per_ccu_color_cache_size = 512 * 1024,
+            sysmem_ccu_depth_cache_fraction = CCUColorCacheFraction.THREE_QUARTER.value,
+            sysmem_per_ccu_depth_cache_size = 512 * 1024,
+#Gmem глубина и цвет 
+            gmem_ccu_color_cache_fraction = CCUColorCacheFraction.EIGHTH.value,
+            gmem_per_ccu_color_cache_size = 256 * 1024,
+            gmem_ccu_depth_cache_fraction = CCUColorCacheFraction.FULL.value,
+            gmem_per_ccu_depth_cache_size = 512 * 1024,
+            
+            #Gmem буфферы
+            gmem_vpc_attr_buf_size = 262144,
+            gmem_vpc_pos_buf_size = 131072,
+            gmem_vpc_bv_pos_buf_size = 131072
+             
+            gmem_size = 18 * 1024 * 1024,
+            has_ray_intersection = False,
+            has_sw_fuse = False,
+            has_coherent_ubwc_flag_caches = True,
+            has_fs_tex_prefetch = False,
+            has_salu_int_narrowing_quirk = True,
+            shading_rate_matches_vk = True)],
         num_ccu = 6,
         num_slices = 3,
         tile_align_w = 96,
