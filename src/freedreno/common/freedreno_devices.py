f3ddc573a043c8bc56405d382fa54b981cd2680b
DVD <dmitrijnemov13@gmail.com>
2026-03-31 09:31:51 +0300

    Update freedreno_devices.py
    Signed-off-by: DVD  <dmitrijnemov13@gmail.com>

--- src/freedreno/common/freedreno_devices.py
+++ src/freedreno/common/freedreno_devices.py
@@ -1372,7 +1372,7 @@ a8xx_gen2 = GPUProps(
 
 add_gpus([
         GPUId(chip_id=0xffff44050000, name="Adreno (TM) 830"),
-        GPUId(chip_id=0x44050001, name="Adreno (TM) 830"), # KGSL
+        GPUId(chip_id=0x44050001, name="Adreno (TM) 830"), 
     ], A6xxGPUInfo(
         CHIP.A8XX,
         [a7xx_base, a7xx_gen3, a8xx_base, a8xx_gen1],
@@ -1400,7 +1400,7 @@ add_gpus([
             sysmem_vpc_attr_buf_size = 131072, 
             sysmem_vpc_pos_buf_size = 65536,
             sysmem_vpc_bv_pos_buf_size = 32768,
-# глубины цветов sysmem
+# глубины цветов sysmem, при больших значениях получается переполнение и черный экран
             sysmem_ccu_color_cache_fraction = CCUColorCacheFraction.FULL.value,
             sysmem_per_ccu_color_cache_size = 32 * 1024,
             sysmem_ccu_depth_cache_fraction = CCUColorCacheFraction.THREE_QUARTER.value,
@@ -1425,15 +1425,15 @@ add_gpus([
          )],
         num_ccu = 2,
         num_slices = 1,
-        tile_align_w = 32,
-        tile_align_h = 16,
-        tile_max_w = 224,
-        tile_max_h = 224,
+        tile_align_w = 64,
+        tile_align_h = 32,
+        tile_max_w = 16384
+        tile_max_h = 16384,
         num_vsc_pipes = 32,
         cs_shared_mem_size = 32 * 1024,
         wave_granularity = 2,
         fibers_per_sp = 128 * 2 * 16,
-    # max_waves = 16 or 32?!!!!
+    # max_waves = 16 or 32?!!!! Надо подумать над этим!
         magic_regs = dict(),
         raw_magic_regs = a8xx_base_raw_magic_regs,
     ))
@@ -1442,6 +1442,7 @@ add_gpus([
 add_gpus([
         GPUId(chip_id=0x44030000, name="Adreno (TM) 825"),
     ], A6xxGPUInfo(
+    #Ждем дополнения от DerixSpace 
         CHIP.A8XX,
         [a7xx_base, a7xx_gen3, a8xx_base, a8xx_gen1, GPUProps(
             # This is probably not an optimal config for gmem/sysmem, but it was working before and I don't have any a825 device to test (neither I have any trace info)
@@ -1486,9 +1487,9 @@ add_gpus([
     
     # Gmem глубина и цвет 
               gmem_ccu_color_cache_fraction = CCUColorCacheFraction.EIGHTH.value,
-              gmem_per_ccu_color_cache_size = 160 * 1024, 
+              gmem_per_ccu_color_cache_size = 64 * 1024, 
               gmem_ccu_depth_cache_fraction = CCUColorCacheFraction.FULL.value,
-              gmem_per_ccu_depth_cache_size = 192 * 1024,
+              gmem_per_ccu_depth_cache_size = 128 * 1024,
 
     # Оптимизации и фичи
              has_ray_intersection = False, # По просьбе группы
@@ -1504,8 +1505,8 @@ add_gpus([
         num_slices = 2,
         tile_align_w = 64,
         tile_align_h = 32,
-        tile_max_w = 256,
-        tile_max_h = 256,
+        tile_max_w = 16384,
+        tile_max_h = 16384,
         num_vsc_pipes = 32,
         cs_shared_mem_size = 32 * 1024,
         wave_granularity = 2,
