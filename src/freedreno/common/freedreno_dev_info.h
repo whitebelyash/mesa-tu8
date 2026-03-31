1d3cd54646961929ce8c260db94cb431b239f825
DVD <dmitrijnemov13@gmail.com>
2026-03-31 08:28:48 +0300

    Add files via upload
    Signed-off-by: DVD  <dmitrijnemov13@gmail.com>

--- src/freedreno/common/freedreno_dev_info.h
+++ src/freedreno/common/freedreno_dev_info.h
@@ -473,6 +473,8 @@ struct fd_dev_info {
       uint32_t max_draw_states;
       /* If GMEM needs to be disabled for this GPU */
       bool disable_gmem;
+      /* GMEM size in bytes */
+      uint32_t gmem_size;
    } props;
 };
 
