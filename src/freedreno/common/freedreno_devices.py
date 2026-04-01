3bb872a4d7dccda65368186570b312126198d248
DVD <dmitrijnemov13@gmail.com>
2026-04-01 11:21:58 +0300

    Update freedreno_devices.py
    Signed-off-by: DVD  <dmitrijnemov13@gmail.com>

--- src/freedreno/common/freedreno_devices.py
+++ src/freedreno/common/freedreno_devices.py
@@ -1506,34 +1506,15 @@ add_gpus([
     ], A6xxGPUInfo(
         CHIP.A8XX,
         [a7xx_base, a7xx_gen3, a8xx_base, a8xx_gen2, GPUProps(
-    #Sysmem кэши (VPC)
-             sysmem_vpc_attr_buf_size = 131072,
-             sysmem_vpc_pos_buf_size = 65536,
+    #Sysmem кэши (VPC)  
              sysmem_vpc_bv_pos_buf_size = 24576,
-            # Sysmem глубина и цвет
-             sysmem_ccu_color_cache_fraction = CCUColorCacheFraction.FULL.value,
-             sysmem_per_ccu_color_cache_size = 128 * 1024,
-             sysmem_ccu_depth_cache_fraction = CCUColorCacheFraction.THREE_QUARTER.value,
-             sysmem_per_ccu_depth_cache_size = 128 * 1024,
-
-            # Gmem кэши (VPC)
-             gmem_vpc_attr_buf_size = 49152,
-             gmem_vpc_pos_buf_size = 24576,     
-             gmem_vpc_bv_pos_buf_size = 16384,  
-    
-    # Gmem глубина и цвет 
-              gmem_ccu_color_cache_fraction = CCUColorCacheFraction.EIGHTH.value,
-              gmem_per_ccu_color_cache_size = 160 * 1024, 
-              gmem_ccu_depth_cache_fraction = CCUColorCacheFraction.FULL.value,
-              gmem_per_ccu_depth_cache_size = 192 * 1024,
-
     # Оптимизации и фичи
              has_ray_intersection = False, # По просьбе группы
              has_sw_fuse = False,
              has_coherent_ubwc_flag_caches = True,
              has_fs_tex_prefetch = False,
              has_salu_int_narrowing_quirk = True,
-             #shading_rate_matches_vk = True, Вызывает мерцания?
+             shading_rate_matches_vk = True, 
              disable_gmem = False,
              gmem_size = 2 * 1024 * 1024
         )],
